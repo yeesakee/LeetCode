83b66cbdfbcd6bffb3c0e86a4911b5bc11694e84
Yeesa Kee <yeesakee@Yeesas-MacBook-Pro.local>
2022-07-05 23:57:48 -0700

    7/5/2022 Completed :)

---
+++ Easy/ClimbingStairs.cpp
@@ -0,0 +1,40 @@
+/*
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. 
+
+** Return how many distinct ways you can climbo to the top **
+
+Example 1:
+    Input: n = 2
+    Output: 2
+    Explanation: There are two ways to climb to the top.
+    1. 1 step + 1 step
+    2. 2 steps
+Example 2:
+    Input: n = 3
+    Output: 3
+    Explanation: There are three ways to climb to the top.
+    1. 1 step + 1 step + 1 step
+    2. 1 step + 2 steps
+    3. 2 steps + 1 step
+
+Constraints:
+   1 = n <= 45
+*/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+    public:
+        int climbStairs(int n) {
+            int one = 1;
+            int two = 1;
+            for (int i = 0; i < n - 1; i++) {
+                int temp = one;
+                one = one + two;
+                two = temp;
+            }
+            return one;
+        }
+};
